891a273ee1d6fbc81838d98a5d73e085d4a8cffe
Urmat <urmat.sharshenov@iaau.edu.kg>
2023-04-22 20:19:19 +0100

    Changed urls

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path("register", views.RegisterAPI.as_view())
+    path("register", views.RegisterAPI.as_view()),
+    path("login/", views.LoginView2.as_view()),
 ]
