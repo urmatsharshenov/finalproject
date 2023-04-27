5857ff31d19ba7f7098ce26def8ac667aa8a9c83
Urmat <urmat.sharshenov@iaau.edu.kg>
2023-04-27 20:19:19 +0100

    Added cors

--- foodDelivery/settings.py
+++ foodDelivery/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "django_filters",
     "users",
     "delivery",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,6 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'foodDelivery.urls'
+
+CORS_ALLOWED_ORIGINS = "*"
 
 TEMPLATES = [
     {
